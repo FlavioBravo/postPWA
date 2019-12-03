c4add21e49a2b4d5ef2d81e8b5404a213fa8594e
Flavio Bravo <f980534860@gmail.com>
2019-12-03 12:08:59 -0500

    done

--- server/push.js
+++ server/push.js
@@ -1,8 +1,11 @@
 
+const fs = require('fs');
+
+
 const urlsafeBase64 = require('urlsafe-base64');
 const vapid = require('./vapid.json');
 
-const suscripciones = [];
+const suscripciones = require('./subs-db.json');
 
 module.exports.getKey = () => {
     return urlsafeBase64.decode( vapid.publicKey );
@@ -11,5 +14,5 @@ module.exports.getKey = () => {
 module.exports.addSubscription = ( suscripcion ) => {
     suscripciones.push( suscripcion );
 
-    console.log( suscripciones );
+    fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify( suscripcion ) );
 };
